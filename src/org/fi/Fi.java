37952641b6ab67bd8257b3038e5eab2f0aa8ecdd
sethuraman.git <seduramanvictory@gmail.com>
2021-06-03 15:29:48 +0530

    br1 added his code

--- src/org/fi/Fi.java
+++ src/org/fi/Fi.java
@@ -3,6 +3,8 @@ package org.fi;
 public class Fi {
 	public static void main(String[] args) {
 		System.out.println("sfvjnlsjdflnv");
+	    System.out.println("automatio");
+	    System.out.println("manual");
 	}
 
 }
